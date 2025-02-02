16af61fa5729d37eca7d2ceb9cc47860457aba7f
Niccolò Cherubin <niccolo.cherubin@ghostprison.onmicrosoft.com>
2025-02-02 16:47:50 +0100

    immagine

--- php/libreria/php/header.php
+++ php/libreria/php/header.php
@@ -21,7 +21,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Gestione Libreria</a>
+        <a class="navbar-brand" href="index.php">Gestione Libreria</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
