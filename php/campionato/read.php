2d4912c28efb2e749e63fc2c30f9640c1975cf99
Niccolò Cherubin <niccolo.cherubin@ghostprison.onmicrosoft.com>
2025-02-15 17:25:51 +0100

    aggiornamento read

--- php/campionato/read.php
+++ php/campionato/read.php
@@ -2,14 +2,11 @@
 include 'php/db.php';
 include 'php/header.php';
 
-$query = "SELECT Piloti.id, Piloti.nome, Piloti.cognome, Piloti.numero, Nazionalita.descrizione AS nazionalita, 
-                 Case_Automobilistiche.nome AS casa_nome, Colore_Livree.descrizione AS livrea 
-          FROM Piloti 
-          JOIN Nazionalita ON Piloti.nazionalita_id = Nazionalita.id
-          JOIN Appartenere ON Piloti.id = Appartenere.Piloti_id
-          JOIN Case_Automobilistiche ON Appartenere.casa_id = Case_Automobilistiche.id
-          JOIN Decidere ON Case_Automobilistiche.id = Decidere.casa_id
-          JOIN Colore_Livree ON Decidere.livrea_id = Colore_Livree.id";
+$query = "SELECT p.id, p.nome, p.cognome, n.descrizione AS nazionalita, p.numero, ca.nome AS casa_nome, cl.descrizione AS livrea 
+          FROM campionato_automobilistico.piloti p
+          JOIN campionato_automobilistico.nazionalita n ON p.nazionalita_id = n.id
+          JOIN campionato_automobilistico.case_automobilistiche ca ON p.casa_id = ca.id 
+          JOIN campionato_automobilistico.colore_livree cl ON ca.livrea_id = cl.id";
 
 $stmt = $pdo->query($query);
 $piloti = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -27,30 +24,28 @@ $piloti = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <th>Nazionalità</th>
             <th>Casa Automobilistica</th>
             <th>Livrea</th>
-            <th>Azioni</th> <!-- Aggiunta colonna per i tasti -->
+            <th>Azioni</th>
         </tr>
         </thead>
         <tbody>
         <?php
         foreach ($piloti as $pilota) {
             echo "<tr>
-            <td>{$pilota['id']}</td>
-            <td>{$pilota['nome']}</td>
-            <td>{$pilota['cognome']}</td>
-            <td>{$pilota['numero']}</td>
-            <td>{$pilota['nazionalita']}</td>
-            <td>{$pilota['casa_nome']}</td>
-            <td>{$pilota['livrea']}</td>
-            <td>
-                <a href='update.php?id={$pilota['id']}' class='btn btn-warning btn-sm'>Modifica</a>
-                <a href='delete.php?id={$pilota['id']}' class='btn btn-danger btn-sm' onclick='return confirm(\"Sei sicuro di voler eliminare questo pilota?\")'>Elimina</a>
-            </td>
-          </tr>";
+                    <td>{$pilota['id']}</td>
+                    <td>{$pilota['nome']}</td>
+                    <td>{$pilota['cognome']}</td>
+                    <td>{$pilota['numero']}</td>
+                    <td>{$pilota['nazionalita']}</td>
+                    <td>{$pilota['casa_nome']}</td>
+                    <td>{$pilota['livrea']}</td>
+                    <td>
+                        <a href='update.php?id={$pilota['id']}' class='btn btn-warning btn-sm'>Modifica</a>
+                        <a href='delete.php?id={$pilota['id']}' class='btn btn-danger btn-sm' onclick='return confirm(\"Sei sicuro di voler eliminare questo pilota?\")'>Elimina</a>
+                    </td>
+                  </tr>";
         }
         ?>
         </tbody>
-
-
     </table>
 </div>
 
