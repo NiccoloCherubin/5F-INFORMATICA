1f4d3586777e71a6d05bf2fbdc3d7ca37d05958b
Niccolò Cherubin <niccolo.cherubin@ghostprison.onmicrosoft.com>
2025-02-20 16:28:11 +0100

    sovrani

---
+++ php/sovrani/index.php
@@ -0,0 +1,11 @@
+<?php
+include 'php/db.php';
+include 'php/header.php';
+?>
+
+<main class="container mt-5 text-center">
+    <h2>Benvenuto al Campionato Automobilistico</h2>
+    <p>Gestisci e visualizza tutte le informazioni relative alle gare, i piloti e le case automobilistiche.</p>
+</main>
+
+<?php include 'php/footer.php'; ?>
